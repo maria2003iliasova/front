7226ddde851d8fe895a4feff924f60d401a36cdc
maria2003iliasova <mia03.03@mail.ru>
2021-10-26 14:50:04 +0300

    first

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+const Home = () => {
+    return (
+        <>
+            <h1>
+                Главная
+            </h1>
+            <p>Алена, работай!</p>
+        </>
+    )
+}
+
+export default Home
